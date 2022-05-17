72831092c47b577f72473ebd08be7da93fd79bec
Zak McRae <zakmcrae@gmail.com>
2022-05-17 09:36:25 -0400

    added routes/controllers, renamed route urls
    added needed delete routes and added routes for post requests from forms
    
    renamed urls to be similar in pattern

--- controllers/index-controller.js
+++ controllers/index-controller.js
@@ -1,23 +1,39 @@
+const database = require("../util/database");
+
 exports.viewHomepage = function (req, res, next) {
   res.render("index", { title: "Welcome to Jess's Baked Goods" });
 };
 
-exports.viewAllItems = function (req, res, next) {
-  res.render("all-items", { title: "View All Items" });
+exports.viewAllItems = async function (req, res, next) {
+  const items = await database.getActiveItems();
+
+  res.render("all-items", { title: "View All Items", items });
 };
 
-exports.viewDeletedItems = function (req, res, next) {
-  res.render("deleted-items", { title: "View Deleted Items" });
+exports.viewDeletedItems = async function (req, res, next) {
+  const items = await database.getDeletedItems();
+
+  res.render("deleted-items", { title: "View Deleted Items", items });
 };
 
 exports.viewItem = function (req, res, next) {
   res.render("item", { title: item.name });
 };
 
-exports.viewCreateItem = function (req, res, next) {
+exports.createItemGet = function (req, res, next) {
   res.render("create-item", { title: "Create New Item" });
 };
 
-exports.viewEditItem = function (req, res, next) {
+exports.createItemPost = function (req, res, next) {};
+
+exports.editItemGet = function (req, res, next) {
   res.render("edit-item", { title: "Create New Item" });
 };
+
+exports.editItemPost = function (req, res, next) {};
+
+exports.deleteItemGet = function (req, res, next) {
+  res.render("delete-item", { title: "Delete Item" });
+};
+
+exports.deleteItemPost = function (req, res, next) {};
--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,31 @@ var router = express.Router();
 // homepage
 router.get("/", indexController.viewHomepage);
 
-// view all items page
-router.get("/all-items", indexController.viewAllItems);
+// view all active items page (non deleted items)
+router.get("/items/all", indexController.viewAllItems);
 
 // view deleted items page
-router.get("/deleted-items", indexController.viewDeletedItems);
+router.get("/items/deleted", indexController.viewDeletedItems);
 
 // view item detail
 router.get("/item/:id", indexController.viewItem);
 
 // view create new item form
-router.get("/create-item", indexController.viewCreateItem);
+router.get("/item/create", indexController.createItemGet);
 
-//view edit item form
-router.get("/edit-item/:id", indexController.viewEditItem);
+// handle create new item form submission
+router.post("/item/create", indexController.createItemPost);
+
+// view edit item form
+router.get("/item/:id/edit", indexController.editItemGet);
+
+// handle edit item form submission
+router.post("/item/:id/edit", indexController.editItemPost);
+
+// view delete item form
+router.get("/item/:id/delete", indexController.deleteItemGet);
+
+// handle delete item form submission
+router.get("/item/:id/delete", indexController.deleteItemPost);
 
 module.exports = router;
